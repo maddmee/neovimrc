9db7dd735a49c055efbb13a93402efcea53193eb
Mohamad <mohamadmostafaee@outlook.com>
2026-02-23 00:38:19 +0330

    Replaced theme and added LSP for Vue

--- init.lua
+++ init.lua
@@ -1,5 +1,7 @@
+vim.g.mapleader = " "
 vim.o.number = true
 vim.o.relativenumber = true
+vim.o.mouse = ""
 vim.o.wrap = false
 vim.o.tabstop = 4
 vim.o.shiftwidth = 4
@@ -8,7 +10,7 @@ vim.o.smartcase = true
 vim.o.ignorecase = true
 vim.o.hlsearch = false
 vim.o.signcolumn = 'yes'
-vim.g.mapleader = vim.keycode('<Space>')
+vim.o.shell = "pwsh"
 
 -- Remap
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
@@ -22,27 +24,31 @@ vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 
--- Package manager 
--- git clone --filter=blob:none https://github.com/nvim-mini/mini.nvim
+-- Package Manager 
 local ok, MiniDeps = pcall(require, 'mini.deps')
 if not ok then
   vim.notify('[WARN] mini.deps module not found', vim.log.levels.WARN)
   return
 end
 MiniDeps.setup()
+
 -- Theme
 MiniDeps.add("folke/tokyonight.nvim")
-require("tokyonight").setup({ style = "storm", transparent = true, })
+require("tokyonight").setup({ style = "moon", transparent = true, })
 vim.cmd.colorscheme("tokyonight")
+
 -- LSP
 MiniDeps.add('neovim/nvim-lspconfig')
 require('mini.snippets').setup()
 require('mini.completion').setup()
 require('mini.comment').setup()
 require('mini.files').setup()
-vim.keymap.set('n', '<leader>e', '<cmd>lua MiniFiles.open()<cr>', {desc = 'File explorer'})
+require('mini.pick').setup()
+vim.keymap.set("n", "<leader>e", function() require("mini.files").open() end, { desc = "File explorer" })
+vim.keymap.set("n", "<leader>ff", function() require("mini.pick").builtin.files() end, { desc = "Find files" })
+vim.keymap.set("n", "<leader>fb", function() require("mini.pick").builtin.buffers() end, { desc = "Find buffers" })
+vim.keymap.set("n", "<leader>fg", function() require("mini.pick").builtin.grep_live() end, { desc = "Live grep" })
+
 -- Language server
--- go install golang.org/x/tools/gopls@latest
-vim.lsp.enable({'gopls'})
--- For rust run: rustup component add rust-analyzer
--- then add the rust_analyzer next to gopls
+-- npm i -g @vtsls/language-server @vue/language-server typescript @vue/typescript-plugin
+vim.lsp.enable({'gopls', 'vtsls', 'vue_ls'})
