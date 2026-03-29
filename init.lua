451a45e8aa9689c437e903f6151e39df31f602c4
Mohamad <mohamadmostafee@outlook.com>
2026-03-29 10:06:09 +0330

    added bootstrap for lsp

--- init.lua
+++ init.lua
@@ -1,79 +1,138 @@
 -- ============================================================================
 -- Bootstrap & Environment
-vim.env.PATH = vim.env.PATH .. ":" .. vim.fn.expand("~/.npm-global/bin")
-vim.g.loaded_netrw, vim.g.loaded_netrwPlugin = 1, 1 
 
-local path_package = vim.fn.stdpath('data') .. '/site' 
-local deps_path = path_package .. '/pack/deps/start/mini.nvim' 
-if not vim.loop.fs_stat(deps_path) 
-	then vim.cmd('echo "Installing mini.nvim (for mini.deps)..." | redraw')
-		vim.fn.system({ 'git', 'clone', '--filter=blob:none', 'https://github.com/nvim-mini/mini.nvim', deps_path, })
-		vim.cmd('packadd mini.nvim | helptags ALL')
-		vim.cmd('echo "Installed mini.nvim" | redraw') 
-	end vim.cmd('packadd mini.nvim') 
+vim.env.PATH = table.concat({
+  vim.env.PATH,
+  vim.fn.expand("~/.npm-global/bin"),
+}, ":")
 
--- ============================================================================
--- Basic Options 
-vim.g.mapleader = " " 
-vim.o.number = true 
-vim.o.relativenumber = true 
-vim.o.wrap = false 
-vim.o.tabstop = 4 
-vim.o.shiftwidth = 4 
-vim.o.scrolloff = 8 
-vim.o.smartcase = true 
-vim.o.ignorecase = true 
-vim.o.hlsearch = false 
-vim.o.signcolumn = 'yes'
+-- disable netrw early
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
 
--- ============================================================================ 
--- Remaps 
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv") 
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv") 
-vim.keymap.set("v", "H", "<gv") 
-vim.keymap.set("v", "L", ">gv") 
-vim.keymap.set("x", "<leader>p", [["_dP]]) 
-vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]]) 
-vim.keymap.set("n", "<leader>y", [["+y]]) 
-vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]]) 
-vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]]) 
-vim.keymap.set("n", "<leader>f", 
-vim.lsp.buf.format) 
+-- paths
+local data = vim.fn.stdpath("data")
+local site = vim.fs.joinpath(data, "site")
+local mini_path = vim.fs.joinpath(site, "pack", "deps", "start", "mini.nvim")
+
+vim.opt.runtimepath:append(site)
+
+-- bootstrap mini.nvim (Neovim 0.12 style)
+if not vim.uv.fs_stat(mini_path) then
+  vim.notify("Installing mini.nvim...", vim.log.levels.INFO)
+
+  vim.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/nvim-mini/mini.nvim",
+    mini_path,
+  }):wait()
+
+  vim.cmd("packadd mini.nvim")
+end
+
+vim.cmd("packadd mini.nvim")
 
 -- ============================================================================
--- Package Manager (mini.deps) 
-local ok, MiniDeps = pcall(require, 'mini.deps') 
-if not ok then vim.notify('[WARN] mini.deps module not found (is mini.nvim installed?)', 
-	vim.log.levels.WARN) 
-	return 
-end 
-MiniDeps.setup() 
-local add = MiniDeps.add 
+-- Basic Options
 
--- ============================================================================ -- Theme 
-add('folke/tokyonight.nvim') require('tokyonight').setup({ style = 'moon', transparent = true, }) 
-vim.cmd.colorscheme('tokyonight')
+vim.g.mapleader = " "
 
--- ============================================================================ 
--- LSP + Mini modules 
-add('neovim/nvim-lspconfig') 
-require('mini.snippets')
-require('mini.completion')
-require('mini.files')
-vim.keymap.set('n', '<leader>e', function() require('mini.files').open() end, { desc = 'File explorer' }) 
-require('mini.pick') 
-vim.keymap.set('n', '<leader>ff', function()
-require('mini.pick').builtin.files() end, { desc = 'Find files' }) 
-vim.keymap.set('n', '<leader>fb', function() require('mini.pick').builtin.buffers() end, { desc = 'Find buffers' }) 
-vim.keymap.set('n', '<leader>fg', function() require('mini.pick').builtin.grep_live() end, { desc = 'Live grep' }) 
-require('mini.icons').setup() 
-require('mini.git').setup() 
-require('mini.cmdline').setup() 
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.wrap = false
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.scrolloff = 8
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.o.hlsearch = false
+vim.o.signcolumn = "yes"
 
--- ============================================================================ 
--- Language servers 
--- npm i -g @vtsls/language-server typescrpit @vue/language-server 
-vim.lsp.enable({ 'gopls', 'vtsls', 'vue_ls' }) 
-vim.lsp.config('vtsls', { 
-	filetypes = { "javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx", "vue", }, 
-}) 
+-- ============================================================================
+-- Keymaps
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "H", "<gv")
+vim.keymap.set("v", "L", ">gv")
+
+vim.keymap.set("x", "<leader>p", [["_dP]])
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
+
+vim.keymap.set("n", "<leader>s",
+  [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]]
+)
+
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+
+-- ============================================================================
+-- mini.deps
+
+local MiniDeps = require("mini.deps")
+MiniDeps.setup()
+
+-- ============================================================================
+-- Plugins
+
+MiniDeps.add("folke/tokyonight.nvim")
+MiniDeps.add("neovim/nvim-lspconfig")
+
+-- mini modules
+require("mini.files").setup()
+require("mini.pick").setup()
+require("mini.icons").setup()
+require("mini.git").setup()
+require("mini.pairs").setup()
+require("mini.surround").setup()
+
+-- UI
+vim.cmd.colorscheme("tokyonight")
+
+-- file explorer
+vim.keymap.set("n", "<leader>e", function()
+  require("mini.files").open()
+end)
+
+-- picker
+vim.keymap.set("n", "<leader>ff", function()
+  require("mini.pick").builtin.files()
+end)
+vim.keymap.set("n", "<leader>fb", function()
+  require("mini.pick").builtin.buffers()
+end)
+vim.keymap.set("n", "<leader>fg", function()
+  require("mini.pick").builtin.grep_live()
+end)
+
+-- ============================================================================
+-- LSP
+
+-- install
+local function ensure(cmd, install_cmd)
+  if vim.fn.executable(cmd) == 0 then
+    vim.notify("Installing " .. cmd)
+    vim.system(install_cmd):wait()
+  end
+end
+
+ensure("gopls", {
+  "go", "install", "golang.org/x/tools/gopls@latest",
+})
+
+ensure("vtsls", {
+  "npm", "i", "-g", "@vtsls/language-server", "typescript",
+})
+
+ensure("vue-language-server", {
+  "npm", "i", "-g", "@vue/language-server",
+})
+
+-- enable
+vim.lsp.enable({
+  "gopls",
+  "vtsls",
+  "vue_ls",
+})
